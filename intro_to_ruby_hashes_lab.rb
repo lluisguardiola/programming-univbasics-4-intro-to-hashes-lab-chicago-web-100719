fd64e39762ac7970199b695bfbdee1f081319b37
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-01 05:45:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,9 @@ def pioneer
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_generator_hash = {
+    :id => 1
+  }
 end
 
 def my_hash_creator(key, value)
