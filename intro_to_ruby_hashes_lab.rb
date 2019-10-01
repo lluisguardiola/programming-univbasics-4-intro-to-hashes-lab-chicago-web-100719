23c6decc63741fdd509e3505ccc94693685e4197
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-01 06:25:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   
   if hash[key]
-    return hash[key]
+    return hash[key] += 1
   else
-    
+    hash[key] = 1
 end
